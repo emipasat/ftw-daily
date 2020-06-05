957e021edad4c522e192c8f1d324937b48c63627
Kimmo Puputti <git@kimmo.kpuputti.fi>
2020-06-05 18:15:11 +0300

    wip: data serialization

--- server/apiServer.js
+++ server/apiServer.js
@@ -8,6 +8,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const sharetribeSdk = require('sharetribe-flex-sdk');
 const apiRouter = require('./apiRouter');
 
 const radix = 10;
